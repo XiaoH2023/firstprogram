26e021632be9e27e805c642e4a1a4ac1f78e3143
Xiao Han <hanxiaoancai@gmail.com>
2023-02-25 14:50:20 -0800

    Animal Class

---
+++ Animal.java
@@ -0,0 +1,18 @@
+class Animal {
+	private String name; 
+	private int age ;
+}
+
+class Dog extends Animal{
+	void bark(){
+		
+		System.out.println("woof");
+
+	}
+}
+
+class JindoDog extends Dog{
+	private String origin; 
+}
+
+
